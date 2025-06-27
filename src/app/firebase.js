ede4a01b597d3a25aff6baf392abb2d42950fe12
trenjoan <88joanferran.ibanez@institutaltpenedes.cat>
2025-06-27 21:55:13 +0200

    Update firebase.js

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -3,7 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
 
 const firebaseConfig = {
-  // Paste your firebase config here
+  apiKey: process.env.apiKey
+  authDomain: process.env.authDomain
+  projectId: process.env.projectId
+  storageBucket: process.env.storageBucket
+  messagingSenderId :process.env.messagingSenderId
+ appId: process.env.appId
 };
 
 // Initialize Firebase
